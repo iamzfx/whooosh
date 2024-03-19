c093bcf52d086223803188397a6e37ff08e2e8d3
Zulfa Mustofa <muhzulfamus@gmail.com>
2024-03-20 01:43:10 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-b8d3d1cf-0063-4369-b6c2-fe7c3f0358ea/)
 
 
 echo "$curl_output" >> Update.tsx
